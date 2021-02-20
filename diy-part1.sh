47a4737d0e5826119686bbb0d39a994157233b43
aoeII <79325762+aoeII@users.noreply.github.com>
2021-02-21 00:08:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+sed -i '$a src-git oaf https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
 
