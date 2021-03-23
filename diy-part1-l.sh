79a14c80c623bcc7a904b96c358e2896ee14e7d4
aoeII <79325762+aoeII@users.noreply.github.com>
2021-03-23 22:26:37 +0800

    Update diy-part1-l.sh

--- diy-part1-l.sh
+++ diy-part1-l.sh
@@ -19,8 +19,8 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git oaf https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
 sed -i '$a src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
-rm -rf package/lean
+# rm -rf package/lean
 cd ..
 git clone https://github.com/coolsnowwolf/lede lede
-mv lede/package/lean openwrt/package/
+cp -rn lede/package/lean/ openwrt/package/lean
 cd openwrt
